7eee24bcd1364b0cf00fe79d106d09e064eaf0a8
sabithmuhammed <91025942+sabithmuhammed@users.noreply.github.com>
2023-09-20 18:55:16 +0530

    nocache

--- router.js
+++ router.js
@@ -1,33 +1,43 @@
-const express=require('express');
-const router=express.Router();
-const credentials={
-    email:'sabith@gmail.com',
-    password:'sabith123'
-}
-const products=require('./data')
-router.post('/login',(req,res)=>{
-    if(req.body.email===credentials.email&&req.body.password===credentials.password){
-        req.session.user=req.body.email;
-        res.redirect('/route/home');
-    }else{
-        res.render('login',{message:'Username or Password is incorrect'});
+const express = require("express");
+const router = express.Router();
+const credentials = {
+  email: "sabith@gmail.com",
+  password: "sabith123",
+};
+const products = require("./data");
+router.post("/login", (req, res) => {
+  if (
+    req.body.email === credentials.email &&
+    req.body.password === credentials.password
+  ) {
+    req.session.user = req.body.email;
+    res.redirect("/route/home");
+  } else {
+    res.render("login", { message: "Username or Password is incorrect" });
+  }
+});
+router.get("/home", (req, res) => {
+  console.log(req.session.user);
+  if (req.session.user) {
+    res.render("home", {
+      user: req.session.user,
+      data: products,
+      title: "home",
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.send("Error");
+    } else {
+      res.render("login", {
+        message: "Logged out successfully",
+        title: "Login",
+      });
     }
-})
-router.get('/home',(req,res)=>{
-    console.log(req.session.user);
-    if(req.session.user){
-        res.render('home',{user:req.session.user,data:products});
-    }else{
-        res.redirect('/');
-    }
-})
-router.get('/logout',(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err){
-            res.send('Error');
-        }else{
-            res.render('login',{message:'Logged out successfully'});
-        }
-    })
-})
-module.exports=router;
+  });
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const express=require('express');
-const path=require('path');
-const bodyparser=require('body-parser');
-const session=require('express-session');
-const {v4:uuidv4}=require('uuid');
-const router=require('./router');
-const app=express();
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const nocache = require("nocache");
+const { v4: uuidv4 } = require("uuid");
+const router = require("./router");
+const app = express();
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}))
-app.use(session({
-    secret:uuidv4(),
-    resave:false,
-    saveUninitialized:true
-}))
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(nocache());
 
-app.set('view engine','ejs')
-app.use('/static',express.static(path.join(__dirname,'public')));
-app.use('/route',router);
-app.get('/',(req,res)=>{
-    res.render('login',{title:'login'});
-})
-
+app.set("view engine", "ejs");
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/route", router);
+app.get("/", (req, res) => {
+  res.render("login", { title: "login" });
+});
 
 app.listen(3000);
